e98c64135898984c7cbd8711b8e9d9277341a6c5
Galleee2002 <gaelgarciasa2002@gmail.com>
2025-04-29 19:26:08 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-blue-500 p-3"></div>
+    </div>
+  );
+}
+
+export default App;
