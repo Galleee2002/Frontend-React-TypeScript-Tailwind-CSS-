bf41a79dd5c7330d4554808b97ae13e86e4926fe
Galleee2002 <gaelgarciasa2002@gmail.com>
2025-04-30 21:02:32 -0300

    added Hero.scss added Hero.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.scss";
 import Button from "./components/button-component/Button";
+import Hero from "./components/button-component/Hero";
 
 function App() {
-  return <div className="App">
-    <Button onClick={()=>console.log("Hola Mundo!")} text="presiona aqui"></Button>
-  </div>;
+  return (
+    <div className="App">
+      {/* <Button
+        onClick={() => console.log("Hola Mundo!")}
+        text="presiona aqui"
+      ></Button> */}
+    </div>
+  );
 }
 
 export default App;
